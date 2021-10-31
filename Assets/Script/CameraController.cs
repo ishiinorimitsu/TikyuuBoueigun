cc8185fef36cff2765364d571e2cc08a2bc8a8ed
norimicia <mitu1023ki0830@gmail.com>
2021-10-31 18:39:01 +0900

    キャラの奥行き、左右の動き実装

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("追跡するゲームオブジェクト")]
+    public CharaController targetObj;
+
+    private Vector3 targetPos;  //targetObjの位置を収納する箱
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();      //カメラを取得
+
+        targetPos = targetObj.transform.position;　　　//targetObjの位置情報を取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = targetObj.transform.position - targetPos;  //ターゲットが移動した分だけカメラも移動する
+
+        targetPos = targetObj.transform.position;
+    }
+}
---
+++ Assets/Script/CharaController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaController : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce = 10;  //ジャンプ力
+
+    [SerializeField]
+    private float moveSpeed = 10;  //水平方向への移動力
+
+    private float moveX;  //x軸方向への移動力
+
+    private float moveZ;  //z軸方向への移動力
+
+    private Rigidbody rb;  //Rigidbodyに力を加えるので、それを入れる。
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal") * moveSpeed;　　//x軸方向への移動量
+
+        moveZ = Input.GetAxis("Vertical") * moveSpeed;     //z軸方向への移動量
+
+        if (Input.GetButtonDown("Jump"))  //スペースキーを押したときにメソッドが発動される。
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    //---------------------------------移動に関する処理----------------------------------------------------------//
+    //水平方向への移動
+    public void Move()
+    {
+        rb.velocity = new Vector3(moveX, 0, moveZ);
+    }
+
+    //ジャンプの移動
+    public void Jump()
+    {
+        rb.AddForce(new Vector3(0, jumpForce *1000,0));
+    }
+}
+
+
