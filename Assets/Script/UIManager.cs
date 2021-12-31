65eb42fbbe0e328ae1d9aa7b0c9b1580febf43e7
norimicia <mitu1023ki0830@gmail.com>
2021-12-31 16:47:30 +0900

    ゲームクリア、ゲームオーバーの条件の実装完了

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -7,9 +7,16 @@ using DG.Tweening;
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
+    private GameManager gameManager;
+
+    //----------------------------------------エネルギー関係----------------------------------------------------//
+
+    [SerializeField]
     private Image energySlider;     //エネルギーゲージを入れる
 
     private float maxEnergyCount;    //最大エネルギー量を入れる
+
+    //-----------------------------------------弾数関係---------------------------------------------------//
 
     [SerializeField]
     private Image bulletSlider;     //残り弾数ゲージを入れる
@@ -22,12 +29,15 @@ public class UIManager : MonoBehaviour
 
     private float maxBulletCount;   //最大弾数を入れる(float)
 
+    //------------------------------------------武器関係--------------------------------------------------//
+
     [SerializeField]
     private Image selectedWeapon;  //選ばれている武器の画像を入れる
 
     [SerializeField]
     private Text selectedWeaponText;  //選ばれている武器のテキストを入れる
 
+    //------------------------------------------HP関係--------------------------------------------------//
 
     [SerializeField]
     private Image HPSlider;    //HPゲージを入れる
@@ -40,6 +50,33 @@ public class UIManager : MonoBehaviour
     private Text maxHP;    //最大HPを入れる。(Text)
 
     private float maxHPCount;    //最大HPを入れる(float)
+
+    //------------------------------------------クリア時のUI関係--------------------------------------------------//
+
+    public CanvasGroup gameClearSet;     //ゲームクリアした時に出すリボンを入れる
+
+    public CanvasGroup clearWindow;     //ゲームのリザルト画面を入れる
+
+    public CanvasGroup gameOverSet;     //GameOver時の文字を入れる。
+
+    public CanvasGroup gameEndBackGround;    //ゲームが終わったときの背景をぼかすもの
+
+    public CanvasGroup gameOverWindow;   //ゲームオーバー時の退却火災挑戦化を選ぶウィンドウを入れる。
+
+
+    //-------------------------------------効果音の設定-------------------------------------//
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip fanfare;     //ゲームをクリアしたときのファンファーレ
+
+    [SerializeField]
+    private AudioClip clearBGM;    //ファンファーレの後に流すBGM
+
+    [SerializeField]
+    private AudioClip failBGM;     //ゲームオーバーの時のBGM
 
 
     //----------------------------------------エネルギーゲージの処理------------------------------------------------------//
@@ -119,5 +156,80 @@ public class UIManager : MonoBehaviour
         HPSlider.fillAmount = currentHp/maxHPCount; //currentHpまで1.0秒かけて動かす（最初の引数の値はmaxHpでいい。）
 
         lastHP.text = currentHp.ToString();   //今のHPを反映させる。数字の更新
+    }
+
+    //-----------------------------------------------------ゲームが終わったときの処理---------------------------------------------//
+
+    /// <summary>
+    /// ゲームクリアの条件を満たしたときにGameClearの画像をファンファーレとともに出す。
+    /// </summary>
+    public void GameClear()
+    {
+        //背景を曇らせる
+        gameEndBackGround.DOFade(1.0f, 1.0f);
+
+        gameManager.SetGameState(GameManager.GameState.end);   //操作できないようにする
+
+        //今流れている曲を止める
+        gameManager.audioSource.Stop();
+
+        //ファンファーレを流す
+        audioSource.PlayOneShot(fanfare);
+
+        //GameClearのリボンを出す
+        gameClearSet.DOFade(endValue:1.0f, duration:2.0f);     //2秒間かけてリボンを見えるようにする
+
+        //５秒くらい待つ
+        StartCoroutine(WaitClearTime());
+    }
+
+    private IEnumerator WaitClearTime()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        //リザルト画面を出す
+        ShowClearResult();
+    }
+
+    private void ShowClearResult()
+    {
+        //ClearResultを出す
+        clearWindow.DOFade(1.0f,1.0f);
+
+        audioSource.PlayOneShot(clearBGM);
+    }
+
+    public void GameOver()
+    {
+        //背景を曇らせる
+        gameEndBackGround.DOFade(1.0f,1.0f);
+
+        gameManager.SetGameState(GameManager.GameState.end);   //操作できないようにする
+
+        //今流れている曲を止める
+        gameManager.audioSource.Stop();
+
+        //失敗のBGMを流す
+        audioSource.PlayOneShot(failBGM);
+
+        gameOverSet.DOFade(endValue: 1.0f, duration: 2.0f);     //2秒間かけてGameOverを見えるようにする
+
+        StartCoroutine(WaitGameOverTime());
+    }
+
+    private IEnumerator WaitGameOverTime()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        //「GameOver」の画面のスイッチを切る（表示させない）
+        gameOverSet.gameObject.SetActive(false);
+
+        //リザルト画面を出す
+        ShowGameOverWindow();
+    }
+
+    public void ShowGameOverWindow()
+    {
+        gameOverWindow.DOFade(1.0f, 1.0f);
     }
 }
